cbd496ce6ac7e0c0489e0c9a30e803a0b0af2501
Addison <addison@wca.cchem.berkeley.edu>
2018-10-03 12:27:53 -0700

    changed verbose default

--- options.py
+++ options.py
@@ -5,7 +5,7 @@ class Options(object):
     Options class for specific non-default options for dynamics classes.
     """
 
-    def __init__(self, verbose=False, progress=True, method='rk4', ntraj=1000, block_avg=False, 
+    def __init__(self, verbose=True, progress=True, method='rk4', ntraj=1000, block_avg=False, 
         nblocks=10, norm_steps=1000, norm_tol=1.e-3, seed=None, markov_time=np.inf):
 
         # program run options #
