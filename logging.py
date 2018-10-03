3124d5007867689877262927e25813e2f37bd138
Addison <addison@wca.cchem.berkeley.edu>
2018-10-03 12:27:34 -0700

    fixed method printing

--- logging.py
+++ logging.py
@@ -1,7 +1,7 @@
 def print_method(line):
     nchar = len(line)
     print("#"*40)
-    print("#"+" "*((40-2-nchar)/2)+line+" "*((40-2-nchar)/2)+"#")
+    print("#"+" "*int((40-2-nchar)/2)+line+" "*int((40-2-nchar)/2)+" #")
     print("#"*40)
     print("")
 
