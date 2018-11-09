bddf18a179a4131b895391e7d43dd55744c9f8f4
Addison <addison@wca.cchem.berkeley.edu>
2018-11-09 13:28:44 -0800

    added dipole moment output

--- spectroscopy.py
+++ spectroscopy.py
@@ -2,36 +2,28 @@ import numpy as np
 import qdynos.constants as const
 from .results import Results
 
-def linear_absorption(times, omegas, mu_op_eig, dynamics):
+def linear_absorption(times, omegas, rho0, mu_op_eig, dynamics):
 
-    dt = times[1]-times[0]
 
     # convert to eigenbasis
     #mu_op_eig = dynamics.ham.to_eigenbasis(mu_op)
     #rho_0 = np.dot(mu_op_eig, dynamics.ham.thermal_dm) - np.dot(dynamics.ham.thermal_dm, mu_op_eig)
-    rho_0 = np.zeros((9,9),dtype=complex)
-    #rho_0[0,0] = 1.
-    rho_0[3,3] = -1.
-    #rho_0[6,6] = -1.
-    rho_0 = dynamics.ham.to_eigenbasis(rho_0)
-    rho_0 = np.dot(mu_op_eig, rho_0)
-    rho_0 = dynamics.ham.from_eigenbasis(rho_0)
+    rho0 = dynamics.ham.to_eigenbasis(rho0)
+    rho0 = np.dot(mu_op_eig, rho0)
+    rho0 = dynamics.ham.from_eigenbasis(rho0)
 
     # run dynamics
     results = Results(tobs=len(times), e_ops=[dynamics.ham.from_eigenbasis(mu_op_eig)])#, print_es=True, es_file='dipole_corr.dat')
-    output = dynamics.solve(rho_0, times, results=results)
-    f = open('dipole_corr.dat','w')
-    for i in range(len(times)):
-        f.write('%.8f %.8f\n'%(times[i],output.expect[0,i]))
-    f.close()
+    output = dynamics.solve(rho0, times, results=results)
 
     # fourier transform of the correlation function
+    dt = times[1]-times[0]
     chi = np.zeros(len(omegas),dtype=complex)
     for i,w in enumerate(omegas):
         corr = np.exp(1.j*w*times)
         chi[i] = (1.j/const.hbar)*np.trapz((corr*output.expect[0,:]),dx=dt)
     
-    return chi
+    return chi , output
 
 #def two_d_ev(times1, times2, times3, omegas_elec, omegas_vib, mu_elec_op, mu_vib_op, dynamics):
 #
