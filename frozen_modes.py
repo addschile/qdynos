1956eaba88c0f0eee4a373909f8fa195777fde94
Addison <addison@wca.cchem.berkeley.edu>
2018-12-14 17:17:13 -0800

    fixed frozen modes, TCL2 works great

--- frozen_modes.py
+++ frozen_modes.py
@@ -6,6 +6,7 @@ from .hamiltonian import Hamiltonian,MDHamiltonian
 from .results import add_results,avg_results
 from .dynamics import Dynamics
 from .unitary import UnitaryDM
+from .log import print_basic
 
 class Frozen:
 
@@ -29,13 +30,14 @@ class Frozen:
             self.dynamics = UnitaryDM(ham)
         else:
             self.dynamics = dynamics
-            if omega_stars==None:
+            if not (isinstance(omega_stars, list) or isinstance(omega_stars, np.ndarray)):
+            #if omega_stars==None:
                 if self.nstates != 2:
                     raise ValueError("Automated omega star only for two-level system.")
                 else:
                     omega_stars = [2.*np.sqrt( 0.25*(H[0,0]-H[1,1])**2. + H[0,1]**2. )/const.hbar]*self.nbath
             if PDs==None:
-                PDs = [True]*self.nbath
+                PDs = [False]*self.nbath
             # compute decomposed spectral densities for each bath #
             for i in range(self.nbath):
                 self.baths[i].frozen_mode_decomp(omega_stars[i], PD=PDs[i])
@@ -43,17 +45,15 @@ class Frozen:
     def solve(self, rho0, times, nmodes=300, ntraj=1000, sample="Boltzmann", results=None):
         results_out = deepcopy(results)
 
-        #avg_ham = np.zeros_like(self.dynamics.ham.ham)
         for traj in range(ntraj):
-            print(traj)
-            dynamics_copy = deepcopy(self.dynamics)#, options=self.options)
+            print_basic(traj)
+            dynamics_copy = deepcopy(self.dynamics)
             baths_copy = [deepcopy(self.baths[i]) for i in range(self.nbath)]
             
             Hb = self.dynamics.ham.ham.copy()
             for i in range(self.nbath):
                 omegas, c_ns, Ps, Qs = self.baths[i].sample_modes(nmodes, sample)
                 Hb += np.sum((c_ns*Qs)[:])*self.baths[i].c_op
-            #avg_ham += Hb
 
             if self.hamtype=="default":
                 dynamics_copy.ham = Hamiltonian(Hb, baths=baths_copy, hbar=self.hbar)
@@ -64,7 +64,7 @@ class Frozen:
             results_i = dynamics_copy.solve(rho, times, results=deepcopy(results))
             results_out = add_results(results_out,results_i)
 
-        return avg_results(ntraj, results_out)# , (avg_ham/float(ntraj))
+        return avg_results(ntraj, results_out)
 
 def frozen_solve(rho0, times, H, baths, hbar=1., omega_stars=None, PDs=None, 
                  nmodes=300, ntraj=1000, sample="Boltzmann", hamtype="default", 
