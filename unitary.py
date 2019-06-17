1f9aea0c0b03f75000642ac401b0dab1c69804a2
Addison Schile <addisonschile@airbears2-10-142-30-124.airbears2.1918.berkeley.edu>
2019-06-17 10:22:53 -0700

    added lanczos and arnoldi propagation

--- unitary.py
+++ unitary.py
@@ -1,8 +1,10 @@
 from __future__ import print_function,absolute_import
 
 import numpy as np
+import scipy.sparse as sp
 from time import time
 from copy import deepcopy
+from scipy.linalg import expm
 
 import qdynos.constants as const
 
@@ -10,7 +12,9 @@ from .integrator import Integrator
 from .dynamics import Dynamics
 from .options import Options
 from .results import Results
-from .log import print_method,print_stage,print_progress,print_time
+from .utils import dag,matmult,norm
+from .linalg import krylov_prop
+from .log import *
 
 class UnitaryDM(Dynamics):
     """
@@ -111,6 +115,7 @@ class UnitaryWF(Dynamics):
         super(UnitaryWF, self).__init__(hamiltonian)
         self.ham = hamiltonian
         print_method("Unitary WF")
+        const.working_units()
 
     def setup(self, options, results):
         """
@@ -128,10 +133,13 @@ class UnitaryWF(Dynamics):
                 assert(str(type(self.ham))=="<class 'qdynos.hamiltonian.MDHamiltonian'>")
                 self.results.map_function = self.ham.compute_coordinate_surfaces
 
-    def eom(self, state, order):
-        return np.dot(self.prop,state)
+    def eom_krylov(self, state, order):
+        return krylov_prop(self.ham.ham, self.options.nlanczos, state, self.dt, self.options.method, lowmem=self.options.lanczos_lowmem)
 
-    def solve(self, psi0, times, options=None, results=None):
+    def eom(self, state, order):
+        return matmult(self.prop, state)
+
+    def solve(self, psi0, times, eig=True, options=None, results=None):
         """
         Solve time-dependent Schrodinger equation for density matrix.
         """
@@ -141,37 +149,56 @@ class UnitaryWF(Dynamics):
         psi = psi0.copy()
 
         if self.options.method == 'exact':
-            for i in range(len(self.results.e_ops)):
-                self.results.e_ops[i] = self.ham.to_eigenbasis(self.results.e_ops[i])
-            psi = self.ham.to_eigenbasis(psi)
-            self.prop = np.diag(np.exp(-(1.j/const.hbar)*self.ham.ev*self.dt))
+            if eig:
+                self.ham.eigensystem()
+                for i in range(len(self.results.e_ops)):
+                    self.results.e_ops[i] = self.ham.to_eigenbasis(self.results.e_ops[i])
+                psi = self.ham.to_eigenbasis(psi)
+                self.prop = np.diag(np.exp(-(1.j/const.hbar)*self.ham.ev*self.dt))
+            else:
+                psi = psi0
+                self.prop = expm(-(1.j/const.hbar)*self.ham.ham*self.dt)
             ode = Integrator(self.dt, self.eom, self.options)
-            ode._set_y_value(psi, times[0])
-            btime = time()
-            print_stage("Propagating Equation of Motion")
-            for i,tau in enumerate(times):
-                if self.options.progress:
-                    if i%int(tobs/10)==0:
-                        etime = time()
-                        print_progress((100*i/tobs),(etime-btime))
-                    elif self.options.really_verbose: print(i)
-                if i%self.results.every==0:
-                    self.results.analyze_state(i, tau, ode.y)
-                ode.integrate()
-        else:
+        elif self.options.method == 'lanczos' or self.options.method == 'arnoldi':
+            # check if relevant matrices are csr matrices #
+            # ham
+            if not isinstance(self.ham.ham, sp.csr_matrix):
+                self.ham.ham = sp.csr_matrix(self.ham.ham)
+            # psi
+            if not isinstance(psi0, sp.csr_matrix):
+                psi = sp.csr_matrix(psi0)
+            # expectation operators
+            if self.results.e_ops != None:
+                for i in range(len(self.results.e_ops)):
+                    if not isinstance(self.results.e_ops[i], sp.csr_matrix):
+                        self.results.e_ops[i] = sp.csr_matrix(self.results.e_ops[i])
+            if self.options.method == 'lanczos' or self.options.method == 'arnoldi':
+                ode = Integrator(self.dt, self.eom_krylov, self.options)
+        elif self.options.method == 'rk4':
+            print_basic("Running dynamics with RK4. This algorithm is far less stable than the others")
+            print_basic("Try setting options.method to lanczos or arnoldi if full diagonalization is prohibited.")
+            psi = psi0
             self.prop = -(1.j/const.hbar)*self.ham.ham
             ode = Integrator(self.dt, self.eom, self.options)
-            ode._set_y_value(psi, times[0])
-            btime = time()
-            print_stage("Propagating Equation of Motion")
-            for i,tau in enumerate(times):
-                if self.options.progress:
+        else:
+            # shouldn't ever get here because of options assert
+            raise ValueError("Incorrect method specification")
+
+        ode._set_y_value(psi, times[0])
+        btime = time()
+        print_stage("Propagating Equation of Motion")
+        for i,tau in enumerate(times):
+            if self.options.progress:
+                if tobs>=10:
                     if i%int(tobs/10)==0:
                         etime = time()
                         print_progress((100*i/tobs),(etime-btime))
-                    elif self.options.really_verbose: print(i)
-                if i%self.results.every==0:
-                    self.results.analyze_state(i, tau, ode.y)
-                ode.integrate()
+                else: 
+                    print(i)
+            if self.options.really_verbose:
+                print(i)
+            if i%self.results.every==0:
+                self.results.analyze_state(i, tau, ode.y)
+            ode.integrate()
 
         return self.results
