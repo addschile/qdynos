73fc6bbc28d85825871c430170286052d21f6574
Addison Schile <addisonschile@airbears2-10-142-30-124.airbears2.1918.berkeley.edu>
2019-06-17 10:21:32 -0700

    added lanczos and arnoldi options, changed lindblad jump options

--- options.py
+++ options.py
@@ -6,13 +6,13 @@ class Options(object):
     """
 
     def __init__(self, verbose=True, really_verbose=False, progress=True,
-                 method="rk4", space="hilbert", print_coup_ops=False, 
-                 coup_ops_file=None, print_decomp=False, decomp_file=None, 
-                 ham_file=None, ntraj=1000, traj_results=False, 
-                 traj_results_file=None, traj_states=False, traj_states_file=None,
-                 traj_states_every=1, block_avg=False, nblocks=10, norm_steps=1000, 
-                 norm_tol=1.e-3, seed=None, markov_time=np.inf, unraveling=False,
-                 which_unraveling='jump', restart_file=None, restart=False):
+                 method="rk4", space="hilbert", norm_tol=0.99, nlanczos=20,
+                 lanczos_lowmem=False, print_coup_ops=False, coup_ops_file=None, 
+                 print_decomp=False, decomp_file=None, ham_file=None, ntraj=1000, 
+                 traj_results=False, traj_results_file=None, traj_states=False, 
+                 traj_states_file=None,traj_states_every=1, block_avg=False, nblocks=10, 
+                 jump_time_steps=1000, jump_time_tol=1.e-3, seed=None, markov_time=np.inf, 
+                 unraveling=False, which_unraveling='jump', restart_file=None, restart=False):
 
         # program run options #
         self.verbose = verbose
@@ -20,8 +20,14 @@ class Options(object):
         self.progress = progress 
 
         # integrator options #
-        assert(method in ['rk4','exact'])
+        assert(method in ['rk4','exact','lanczos','arnoldi'])
         self.method = method
+
+        # unitary evolution options #
+        # TODO check default, that might be a bit ridiculous
+        self.norm_tol = norm_tol
+        self.nlanczos = nlanczos
+        self.lanczos_lowmem = lanczos_lowmem
 
         # liouville or hilbert space #
         self.space = space
@@ -61,9 +67,10 @@ class Options(object):
         self.which_unraveling = which_unraveling
         if self.unraveling:
             if self.which_unraveling=='jump':
-                self.method = 'exact'
-        self.norm_steps = norm_steps
-        self.norm_tol = norm_tol
+                if self.method == 'rk4':
+                    self.method = 'exact'
+        self.jump_time_steps = jump_time_steps
+        self.jump_time_tol = jump_time_tol
 
         # NOTE in progress: restart options #
         self.restart_file = restart_file
