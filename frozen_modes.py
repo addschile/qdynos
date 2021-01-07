d025721a27dfd723f2412a62e8a0f293d60eef94
Addison Schile <addschile@gmail.com>
2021-01-07 12:48:52 -0800

    four spaces good two spaces better

--- frozen_modes.py
+++ frozen_modes.py
@@ -12,162 +12,162 @@ from .log import print_basic,print_progress
 
 class Frozen:
 
-    def __init__(self, H, baths, nstates=None, hbar=1., omega_stars=None,
-                 PDs=None, hamtype="default", dynamics=None, options=None):
-        self.nbath = len(baths)
-        if nstates == None:
-            self.nstates = H.shape[0]
+  def __init__(self, H, baths, nstates=None, hbar=1., omega_stars=None,
+         PDs=None, hamtype="default", dynamics=None, options=None):
+    self.nbath = len(baths)
+    if nstates == None:
+      self.nstates = H.shape[0]
+    else:
+      self.nstates = nstates
+    self.baths = baths
+    if options==None:
+      self.options = Options()
+    else:
+      self.options = options
+    self.hamtype = hamtype
+    self.hbar = hbar
+    self.omega_stars = omega_stars
+
+    if dynamics==None:
+      if hamtype=="default":
+          self.ham = Hamiltonian(H, nstates=nstates, hbar=hbar)
+      elif hamtype=="md":
+          self.ham = MDHamiltonian(H)
+      self.dynamics = UnitaryDM(ham)
+    else:
+      self.dynamics = dynamics
+      if not (isinstance(omega_stars, list) or isinstance(omega_stars, np.ndarray)):
+        if self.nstates != 2:
+          raise ValueError("Automated omega star only for two-level system.")
         else:
-            self.nstates = nstates
-        self.baths = baths
-        if options==None:
-            self.options = Options()
-        else:
-            self.options = options
-        self.hamtype = hamtype
-        self.hbar = hbar
-        self.omega_stars = omega_stars
+          omega_stars = [0.5*np.sqrt( 0.25*(H[0,0]-H[1,1])**2. + H[0,1]**2. )/const.hbar]*self.nbath
+      if PDs==None:
+        PDs = [False]*self.nbath
+      # compute decomposed spectral densities for each bath #
+      for i in range(self.nbath):
+        if omega_stars[i] != 0:
+          self.baths[i].frozen_mode_decomp(omega_stars[i], PD=PDs[i])
 
-        if dynamics==None:
-            if hamtype=="default":
-                    self.ham = Hamiltonian(H, nstates=nstates, hbar=hbar)
-            elif hamtype=="md":
-                    self.ham = MDHamiltonian(H)
-            self.dynamics = UnitaryDM(ham)
-        else:
-            self.dynamics = dynamics
-            if not (isinstance(omega_stars, list) or isinstance(omega_stars, np.ndarray)):
-                if self.nstates != 2:
-                    raise ValueError("Automated omega star only for two-level system.")
-                else:
-                    omega_stars = [0.5*np.sqrt( 0.25*(H[0,0]-H[1,1])**2. + H[0,1]**2. )/const.hbar]*self.nbath
-            if PDs==None:
-                PDs = [False]*self.nbath
-            # compute decomposed spectral densities for each bath #
-            for i in range(self.nbath):
-                if omega_stars[i] != 0:
-                    self.baths[i].frozen_mode_decomp(omega_stars[i], PD=PDs[i])
+  def sample_hamiltonians(self, nmodes=300, ntraj=1000, sample="Boltzmann"):
+    for traj in range(ntraj):
+      Hb = self.dynamics.ham.ham.copy()
+      if self.options.print_decomp:
+        self.options.decomp_file = open(self.options._decomp_file+"_traj_%d"%(traj), "w")
+      for i in range(self.nbath):
+        if self.omega_stars[i] != 0:
+          omegas, c_ns, Ps, Qs = self.baths[i].sample_modes(nmodes, sample)
+          if self.options.print_decomp:
+            for j in range(len(c_ns)):
+              self.options.decomp_file.write("%.64f %.64f %.64f %.64f\n"%(omegas[j],c_ns[j],Ps[j],Qs[j]))
+              self.options.decomp_file.flush()
+          Hb += np.sum((c_ns*Qs)[:])*self.baths[i].c_op
+      if self.options.print_decomp:
+        if self.options.ham_file != None:
+          np.save(self.options.ham_file+'_traj_%d'%(traj),Hb)
+        self.options.decomp_file.close()
 
-    def sample_hamiltonians(self, nmodes=300, ntraj=1000, sample="Boltzmann"):
-        for traj in range(ntraj):
-            Hb = self.dynamics.ham.ham.copy()
-            if self.options.print_decomp:
-                self.options.decomp_file = open(self.options._decomp_file+"_traj_%d"%(traj), "w")
-            for i in range(self.nbath):
-                if self.omega_stars[i] != 0:
-                    omegas, c_ns, Ps, Qs = self.baths[i].sample_modes(nmodes, sample)
-                    if self.options.print_decomp:
-                        for j in range(len(c_ns)):
-                            self.options.decomp_file.write("%.64f %.64f %.64f %.64f\n"%(omegas[j],c_ns[j],Ps[j],Qs[j]))
-                            self.options.decomp_file.flush()
-                    Hb += np.sum((c_ns*Qs)[:])*self.baths[i].c_op
-            if self.options.print_decomp:
-                if self.options.ham_file != None:
-                    np.save(self.options.ham_file+'_traj_%d'%(traj),Hb)
-                self.options.decomp_file.close()
+  def solve(self, rho0, times, nmodes=300, ntraj=1000, sample="Boltzmann", results=None):
+    results_out = deepcopy(results)
 
-    def solve(self, rho0, times, nmodes=300, ntraj=1000, sample="Boltzmann", results=None):
-        results_out = deepcopy(results)
+    every = None
+    if ntraj >= 10: every = int(ntraj/10)
+    else: every = 1
+    btime = time()
+    for traj in range(ntraj):
+      if traj%every==0:
+        print_progress(10*traj/every,time()-btime)
+      dynamics_copy = deepcopy(self.dynamics)
+      baths_copy = [deepcopy(self.baths[i]) for i in range(self.nbath)]
+      
+      Hb = self.dynamics.ham.ham.copy()
+      if self.options.print_decomp:
+        self.options.decomp_file = open(self.options._decomp_file+"_traj_%d"%(traj), "w")
+      for i in range(self.nbath):
+        if self.omega_stars[i] != 0:
+          omegas, c_ns, Ps, Qs = self.baths[i].sample_modes(nmodes, sample)
+          if self.options.print_decomp:
+            for j in range(len(c_ns)):
+              self.options.decomp_file.write("%.64f %.64f %.64f %.64f\n"%(omegas[j],c_ns[j],Ps[j],Qs[j]))
+              self.options.decomp_file.flush()
+          print_basic("reorganization energy: %.16f\n"%(np.sum((c_ns*Qs)[:])))
+          Hb += np.sum((c_ns*Qs)[:])*self.baths[i].c_op
+      if self.options.print_decomp:
+        if self.options.ham_file != None:
+          np.save(self.options.ham_file+'_traj_%d'%(traj),Hb)
+        self.options.decomp_file.close()
 
-        every = None
-        if ntraj >= 10: every = int(ntraj/10)
-        else: every = 1
-        btime = time()
-        for traj in range(ntraj):
-            if traj%every==0:
-                print_progress(10*traj/every,time()-btime)
-            dynamics_copy = deepcopy(self.dynamics)
-            baths_copy = [deepcopy(self.baths[i]) for i in range(self.nbath)]
-            
-            Hb = self.dynamics.ham.ham.copy()
-            if self.options.print_decomp:
-                self.options.decomp_file = open(self.options._decomp_file+"_traj_%d"%(traj), "w")
-            for i in range(self.nbath):
-                if self.omega_stars[i] != 0:
-                    omegas, c_ns, Ps, Qs = self.baths[i].sample_modes(nmodes, sample)
-                    if self.options.print_decomp:
-                        for j in range(len(c_ns)):
-                            self.options.decomp_file.write("%.64f %.64f %.64f %.64f\n"%(omegas[j],c_ns[j],Ps[j],Qs[j]))
-                            self.options.decomp_file.flush()
-                    print_basic("reorganization energy: %.16f\n"%(np.sum((c_ns*Qs)[:])))
-                    Hb += np.sum((c_ns*Qs)[:])*self.baths[i].c_op
-            if self.options.print_decomp:
-                if self.options.ham_file != None:
-                    np.save(self.options.ham_file+'_traj_%d'%(traj),Hb)
-                self.options.decomp_file.close()
+      if self.hamtype=="default":
+        dynamics_copy.ham = Hamiltonian(Hb, nstates=self.nstates, baths=baths_copy, hbar=self.hbar)
+      elif self.hamtype=="md":
+        dynamics_copy.ham = MDHamiltonian(Hb, baths=baths_copy, hbar=self.hbar)
 
-            if self.hamtype=="default":
-                dynamics_copy.ham = Hamiltonian(Hb, nstates=self.nstates, baths=baths_copy, hbar=self.hbar)
-            elif self.hamtype=="md":
-                dynamics_copy.ham = MDHamiltonian(Hb, baths=baths_copy, hbar=self.hbar)
+      rho = rho0.copy()
+      traj_results = deepcopy(results)
+      if self.options.print_coup_ops:
+        dynamics_copy.options.print_coup_ops = True
+        dynamics_copy.options.coup_ops_file = self.options.coup_ops_file + "_traj_%d"%(traj)
+      if self.options.traj_results:
+        traj_results.print_es = True
+        traj_results.fes = open(self.options.traj_results_file+"_traj_%d"%(traj), "w")
+      if self.options.traj_states:
+        traj_results.print_states = True
+        traj_results.states_file = self.options.traj_states_file+"_traj_%d_"%(traj)
+        traj_results.states_every = self.options.traj_states_every
+      results_i = dynamics_copy.solve(rho, times, results=traj_results)
+      results_out = add_results(results_out,results_i)
 
-            rho = rho0.copy()
-            traj_results = deepcopy(results)
-            if self.options.print_coup_ops:
-                dynamics_copy.options.print_coup_ops = True
-                dynamics_copy.options.coup_ops_file = self.options.coup_ops_file + "_traj_%d"%(traj)
-            if self.options.traj_results:
-                traj_results.print_es = True
-                traj_results.fes = open(self.options.traj_results_file+"_traj_%d"%(traj), "w")
-            if self.options.traj_states:
-                traj_results.print_states = True
-                traj_results.states_file = self.options.traj_states_file+"_traj_%d_"%(traj)
-                traj_results.states_every = self.options.traj_states_every
-            results_i = dynamics_copy.solve(rho, times, results=traj_results)
-            results_out = add_results(results_out,results_i)
-
-        return avg_results(ntraj, results_out)
+    return avg_results(ntraj, results_out)
 
 # NOTE I'm only keeping this around, just in case I decide it's useful
 #def frozen_solve(rho0, times, H, baths, hbar=1., omega_stars=None, PDs=None, 
-#                 nmodes=300, ntraj=1000, sample="Boltzmann", hamtype="default", 
-#                 hamargs=None, dynamics=None, options=None, results=None):
-#    """
-#    """
-#    nbath = len(baths)
-#    nstates = H.shape[0]
-#    const.hbar = hbar
+#         nmodes=300, ntraj=1000, sample="Boltzmann", hamtype="default", 
+#         hamargs=None, dynamics=None, options=None, results=None):
+#  """
+#  """
+#  nbath = len(baths)
+#  nstates = H.shape[0]
+#  const.hbar = hbar
 #
-#    if dynamics==None:
-#        if hamtype=="default":
-#                ham = Hamiltonian(H)
-#        elif hamtype=="md":
-#                ham = MDHamiltonian(H)
-#        dynamics = UnitaryDM(ham)
-#    else:
-#        if omega_stars==None:
-#            if nstates != 2:
-#                raise ValueError("Automated omega star only for two-level system.")
-#            else:
-#                omega_stars = [2.*np.sqrt( 0.25*(H[0,0]-H[1,1])**2. + H[0,1]**2. )/const.hbar]*nbath
+#  if dynamics==None:
+#    if hamtype=="default":
+#        ham = Hamiltonian(H)
+#    elif hamtype=="md":
+#        ham = MDHamiltonian(H)
+#    dynamics = UnitaryDM(ham)
+#  else:
+#    if omega_stars==None:
+#      if nstates != 2:
+#        raise ValueError("Automated omega star only for two-level system.")
+#      else:
+#        omega_stars = [2.*np.sqrt( 0.25*(H[0,0]-H[1,1])**2. + H[0,1]**2. )/const.hbar]*nbath
 #
-#        if PDs==None:
-#            PDs = [True]*nbath
+#    if PDs==None:
+#      PDs = [True]*nbath
 #
-#        # compute decomposed spectral densities for each bath #
-#        for i in range(nbath):
-#            baths[i].frozen_mode_decomp(omega_stars[i], PD=PDs[i])
+#    # compute decomposed spectral densities for each bath #
+#    for i in range(nbath):
+#      baths[i].frozen_mode_decomp(omega_stars[i], PD=PDs[i])
 #
-#    results_out = deepcopy(results)
+#  results_out = deepcopy(results)
 #
-#    avg_ham = np.zeros_like(H)
-#    for traj in range(ntraj):
-#        dynamics_copy = deepcopy(dynamics)
-#        baths_copy = [deepcopy(baths[i]) for i in range(nbath)]
-#        
-#        Hb = H.copy()
-#        for i in range(nbath):
-#            omegas, c_ns, Ps, Qs = baths[i].sample_modes(nmodes, sample)
-#            Hb += np.sum((c_ns*Qs)[:])*baths[i].c_op
-#        avg_ham += Hb
+#  avg_ham = np.zeros_like(H)
+#  for traj in range(ntraj):
+#    dynamics_copy = deepcopy(dynamics)
+#    baths_copy = [deepcopy(baths[i]) for i in range(nbath)]
+#    
+#    Hb = H.copy()
+#    for i in range(nbath):
+#      omegas, c_ns, Ps, Qs = baths[i].sample_modes(nmodes, sample)
+#      Hb += np.sum((c_ns*Qs)[:])*baths[i].c_op
+#    avg_ham += Hb
 #
-#        if self.hamtype=="default":
-#            dynamics_copy.ham = Hamiltonian(Hb, baths=baths_copy, hbar=hbar)
-#        elif self.hamtype=="md":
-#            dynamics_copy.ham = MDHamiltonian(Hb, baths=baths_copy, hbar=hbar)
+#    if self.hamtype=="default":
+#      dynamics_copy.ham = Hamiltonian(Hb, baths=baths_copy, hbar=hbar)
+#    elif self.hamtype=="md":
+#      dynamics_copy.ham = MDHamiltonian(Hb, baths=baths_copy, hbar=hbar)
 #
-#        rho = rho0.copy()
-#        results_i = dynamics_copy.solve(rho, times, options=options, results=deepcopy(results))
-#        results_out = add_results(results_out,results_i)
+#    rho = rho0.copy()
+#    results_i = dynamics_copy.solve(rho, times, options=options, results=deepcopy(results))
+#    results_out = add_results(results_out,results_i)
 #
-#    return avg_results(ntraj, results_out) , (avg_ham/float(ntraj))
+#  return avg_results(ntraj, results_out) , (avg_ham/float(ntraj))
