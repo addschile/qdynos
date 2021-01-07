0d1de7a87b4c03160775cb0d78fae3cf8c2c5573
Addison Schile <addschile@gmail.com>
2021-01-07 12:47:46 -0800

    four spaces good two spaces better

--- constants.py
+++ constants.py
@@ -25,39 +25,39 @@ ps2au = fs2au/1000. # au/ps
 fs2ps = 1./1000. # ps/fs
 
 def working_units():
-    from numpy import isclose 
-    if hbar==None:
-        print("No working units assigned\n")
-    elif isclose(hbar,5308.8):
-        print("ENERGY UNITS: cm^-1\nTIME UNITS: fs\n")
-    elif isclose(hbar,0.658229):
-        print("ENERGY UNITS: eV\nTIME UNITS: fs\n")
-    elif hbar==1.0:
-        print("ENERGY UNITS: au\nTIME UNITS: au\n")
-    else:
-        print("Unrecognized units system\n")
+  from numpy import isclose 
+  if hbar==None:
+    print("No working units assigned\n")
+  elif isclose(hbar,5308.8):
+    print("ENERGY UNITS: cm^-1\nTIME UNITS: fs\n")
+  elif isclose(hbar,0.658229):
+    print("ENERGY UNITS: eV\nTIME UNITS: fs\n")
+  elif hbar==1.0:
+    print("ENERGY UNITS: au\nTIME UNITS: au\n")
+  else:
+    print("Unrecognized units system\n")
 
 # TODO
 #def convert_units():
     
 def get_hbar(*units):
-    if len(units)==0:
-        return hbars['au']
-    else:
-        try:
-            assert(units[0].lower() in ['ev','cm','au'])
-        except:
-            raise ValueError('Not a valid units system')
-        return hbars[units[0].lower()]
+  if len(units)==0:
+    return hbars['au']
+  else:
+    try:
+      assert(units[0].lower() in ['ev','cm','au'])
+    except:
+      raise ValueError('Not a valid units system')
+    return hbars[units[0].lower()]
 
 # TODO this isn't working
 def set_hbar(*units):
-    if len(units)==0:
-        hbar = hbars['au']
-    else:
-        try:
-            assert(units[0].lower() in ['ev','cm','au'])
-        except:
-            raise ValueError('Not a valid units system')
-        hbar = hbars[units[0].lower()]
+  if len(units)==0:
+    hbar = hbars['au']
+  else:
+    try:
+      assert(units[0].lower() in ['ev','cm','au'])
+    except:
+      raise ValueError('Not a valid units system')
+    hbar = hbars[units[0].lower()]
 
