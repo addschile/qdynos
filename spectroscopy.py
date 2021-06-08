8db5bd2c77b809e388df76cc07dcec7c825ae6e3
Addison Schile <addschile@gmail.com>
2021-06-07 21:04:38 -0700

    updated 2dev function, but still doesn't work

--- spectroscopy.py
+++ spectroscopy.py
@@ -25,23 +25,29 @@ def linear_absorption(times, omegas, rho0, mu_op_eig, dynamics, print_dipole=Tru
     
     return chi
 
-#def two_d_ev(times1, times2, times3, omegas_elec, omegas_vib, mu_elec_op, mu_vib_op, dynamics):
-#
-#    dt = times[1]-times[0]
-#
-#    # convert to eigenbasis
-#    mu_op_eig = dynamics.ham.to_eigenbasis(mu_op)
-#    rho_0 = np.dot(mu_op_eig, dynamics.ham.thermal_dm) - np.dot(dynamics.ham.thermal_dm, mu_op_eig)
-#    rho_0 = dynamics.ham.to_eigenbasis(rho_0)
-#
-#    # run dynamics
-#    results = Results(tobs=len(times), e_ops=mu_op)
-#    output = dynamics.solve(times, rho_0, results=Results)
-#
-#    # fourier transform of the correlation function
-#    corr = np.exp(1.j*np.outer(omegas*times))
-#    chi = np.zeros(len(omegas),dtype=complex)
-#    for i,w in enumerate(omegas):
-#        chi[i] = (1.j/const.hbar)*np.trapz(corr[i,:]*output.expect[0,:],dx=dt)
-#    
-#    return chi.imag
+def two_d_ev(times1, times2, times3, omegas_e, omegas_v, mu_e, mu_v, dynamics):
+    """
+    """
+    dt = times1[1]-times1[0]
+    corr = np.zeros(4, dtype=np.ndarray)
+    
+    # first pathway
+    mu_e_eig = dynamics.ham.to_eigenbasis(mu_e)
+    rho_0 = np.dot(mu_op_eig, dynamics.ham.thermal_dm)
+    rho_0 = dynamics.ham.from_eigenbasis(rho_0)
+    for i in range(len(times1)):
+        _times1 = np.arange(times1[0],times[i],dt)
+        for j in range(len(times2)):
+            for k in range(len(times3)):
+
+    # run dynamics
+    results = Results(tobs=len(times), e_ops=mu_op)
+    output = dynamics.solve(times, rho_0, results=Results)
+
+    # fourier transform of the correlation function
+    corr = np.exp(1.j*np.outer(omegas*times))
+    chi = np.zeros(len(omegas),dtype=complex)
+    for i,w in enumerate(omegas):
+        chi[i] = (1.j/const.hbar)*np.trapz(corr[i,:]*output.expect[0,:],dx=dt)
+    
+    return chi.imag
